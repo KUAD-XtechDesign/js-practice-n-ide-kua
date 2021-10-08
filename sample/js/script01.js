cc483d3d8d496cbdc81d604b3cf82530a84c1954
ide-natsuki <n-ide6270@st.kyoto-art.ac.jp>
2021-10-08 14:46:46 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,5 +1,11 @@
  $(function(){
-  $("#button01").on("click",function(){
+
+   $("#button01").on("click",function(){
     $(".back-red").hide();
-  })
+   })
+
+   $("#button02").on("clisk",function(){
+       $(".back-blue").hide();
+   })
+
  })
